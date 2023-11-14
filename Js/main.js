84d3db3ed89d26cc2a8635b24c3eefd2f715fc95
JosseJuventino <00048322@uca.edu.sv>
2023-11-14 11:12:47 -0600

    adding view 2

--- Js/main.js
+++ Js/main.js
@@ -1,59 +1,76 @@
+
 let BotonConvertir = document.getElementById("BotonConvertir");
 let arrayPersonas = [];
+
+let jsConfetti = new JSConfetti();
+
+
+let contenedorPage = document.getElementById("contenedor");
+let buttonDownloadSection = document.getElementById("buttonDownload"); 
+
+let InsertsDownloadButton = document.getElementById("InsertsDownload");
+let UsersDownloadButton = document.getElementById("UsersDownload");
+let restoreDownloadButton = document.getElementById("restoreDownload");
+let returnButton = document.getElementById("returnButton");
+
 BotonConvertir.addEventListener("click", function () {
-    leerArchivo();
+    /** Acá se ejecuta cuando se da click a ejecutar */
+    LeerArchivo();
+    contenedorPage.style.display = "none";
+    buttonDownloadSection.style.display = "block";
 });
 
-function convertir(content) {
-    contenidoo = content.replace(/\r\n/g, "\n");
-    let lineas = contenidoo.split("\n");
-    for (let i = 0; i < lineas.length; i++) {
-        if (lineas[i]) {
-            let campos = lineas[i].split(";");
-
-            let persona = {
-                nombre: campos[0],
-                tipo: campos[1],
-                carnet: campos[2],
-                correo: campos[3],
-            };
-            arrayPersonas = [...arrayPersonas, persona];
-        }
-    }
-
-    CreandoSentenciaSQL(arrayPersonas);
-}
-
-function leerArchivo() {
+function LeerArchivo() {
     let archivo = document.getElementById("file").files[0];
     if (archivo) {
         let reader = new FileReader();
         reader.readAsText(archivo, "UTF-8");
         reader.onload = function (evt) {
             let contenido = evt.target.result;
-            convertir(contenido);
+            CrearObjetoPersona(contenido);
         };
         reader.onerror = function (evt) {
-            console.log("error al leer el archivo");
+            console.log("Ocurrio un error al leer el archivo :/");
         };
     } else {
-        alert("No se ha cargado ningun archivo");
+        alert("Por favor seleccione un archivo >:v");
     }
 }
 
-function CrearAchivoSQL(sqlSentence,nombreArchivo) {
-    let texto = new Blob([sqlSentence], { type: "text/sql" });
-    let archivo = document.createElement("a");
-    archivo.download = nombreArchivo;
-    archivo.href = URL.createObjectURL(texto);
-    archivo.click();
+function CrearObjetoPersona(content) {
+    textDepurado = content.replace(/\r\n/g, "\n");
+    let lineas = textDepurado.split("\n");
+
+    for (let i = 0; i < lineas.length; i++) {
+        if (lineas[i]) {
+            let campos = lineas[i].split(";");
+
+            //Convirtiendo en un array de objetos
+            let persona = {
+                nombre: campos[0],
+                tipo: campos[1],
+                carnet: campos[2],
+                correo: campos[3],
+            };
+
+            //Agregando al array de personas
+            arrayPersonas = [...arrayPersonas, persona];
+        }
+    }
+
+    //Una vez se tiene el array de personas separadas por linea y por puntos y comas, 
+    //se procede a crear el archivo SQL
+    CreandoSentenciaSQL(arrayPersonas);
 }
 
 function CreandoSentenciaSQL(array) {
+    // Variables que almacenan la consulta SQL
     let sqlSentence = "";
     let sqlUser = "";
-    let sqlDeleteUser="";
-        for (let i = 0; i < array.length; i++) {
+    let sqlDeleteUser = "";
+
+    //Recorriendo el archivo
+    for (let i = 0; i < array.length; i++) {
         if (array[i].tipo == "Estudiante") {
             sqlUser += `CREATE USER U_${array[i].carnet} IDENTIFIED BY ${generarContrasena(10)} DEFAULT TABLESPACE t_estudiantes TEMPORARY TABLESPACE temp QUOTA UNLIMITED ON t_estudiantes;\n`;
             sqlSentence += `INSERT INTO ESTUDIANTE (nombre, carnet, correo) VALUES ('${array[i].nombre}', '${array[i].carnet}', '${array[i].correo}');\n`;
@@ -72,9 +89,39 @@ function CreandoSentenciaSQL(array) {
             sqlDeleteUser += `DROP USER U_${array[i].carnet};\n`;
         }
     }
-    CrearAchivoSQL(sqlSentence, "Inserts.sql");
-    CrearAchivoSQL(sqlUser, "Users.sql");
-    CrearAchivoSQL(sqlDeleteUser, "DeleteUsers.sql");
+    
+
+    InsertsDownloadButton.addEventListener("click", function () {
+        CrearAchivoSQL(sqlSentence, "Inserts.sql");
+    });
+    
+    UsersDownloadButton.addEventListener("click", function () {
+        CrearAchivoSQL(sqlUser, "Users.sql");
+    });
+    
+    restoreDownloadButton.addEventListener("click", function () {
+        CrearAchivoSQL(sqlDeleteUser, "restore.sql");
+    });
+
+  
+    jsConfetti.addConfetti({
+        emojis: ['🥵','🔥'],
+     })
+}
+
+
+
+returnButton.addEventListener("click", function () {
+    contenedorPage.style.display = "block";
+    buttonDownloadSection.style.display = "none";
+});
+
+function CrearAchivoSQL(sqlSentence, nombreArchivo) {
+    let texto = new Blob([sqlSentence], { type: "text/sql" });
+    let archivo = document.createElement("a");
+    archivo.download = nombreArchivo;
+    archivo.href = URL.createObjectURL(texto);
+    archivo.click();
 }
 
 function generarContrasena(longitud) {
